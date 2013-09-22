4921f154cd39af67a833b44d9f98afc88b93d71d
Kat Marchán <kzm@sykosomatic.org>
2013-09-22 15:14:05 -0700

    Let text always succeed with at least an empty string

--- src/mona.js
+++ src/mona.js
@@ -449,14 +449,14 @@ function spaces() {
 }
 
 /**
- * Returns a parser that collects one or more tokens matching `parser`. Thim
+ * Returns a parser that collects zero or more tokens matching `parser`. Thim
  * result is returned as a single string.
  *
  * @param {Parser} [parser=token()] - Parser to use to collect thim results.
  */
 function text(parser) {
   parser = parser || token();
-  return stringOf(oneOrMore(parser));
+  return stringOf(zeroOrMore(parser));
 }
 
 /*
