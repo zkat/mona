50ffade89c483850efd23876c5f12e79d69b1348
Kat Marchán <kzm@sykosomatic.org>
2013-09-29 20:48:40 -0700

    Short example of why delay() is useful

--- src/mona.js
+++ src/mona.js
@@ -319,6 +319,16 @@ function eof() {
  * @param {...Any} args - Arguments to apply to the constructor.
  * @returns {core.Parser}
  * @memberof core
+ *
+ * @example
+ * // The following would usually result in an infinite loop:
+ * function foo() {
+ *   return or(x(), foo());
+ * }
+ * // But you can use delay() to remedy this...
+ * function foo() {
+ *   return or(x(), delay(foo));
+ * }
  */
 function delay(constructor) {
   var args = [].slice.call(arguments, 1);
