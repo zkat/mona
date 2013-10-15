bd6219bd6f99801fcf3ba0b629b83334409f74c0
Kat Marchán <kzm@sykosomatic.org>
2013-10-14 19:42:00 -0700

    More better noneOf()

--- src/mona-test.js
+++ src/mona-test.js
@@ -641,13 +641,26 @@ describe("mona", function() {
         assert.equal(parse(mona.noneOf("abc"), "d"), "d");
         assert.throws(function() {
           parse(mona.noneOf("abc"), "b");
-        }, /expected none of {abc}/);
+        }, /expected none of {a,b,c}/);
+      });
+      it("accepts an array of strings as matches", function() {
+        assert.equal(parse(mona.noneOf(["foo", "bar"]), "x"), "x");
+        assert.throws(function() {
+          parse(mona.noneOf(["foo", "bar"]), "foo");
+        }, /expected none of {foo,bar}/);
+      });
+      it("accepts a parser that will run if matches fail", function() {
+        assert.equal(parse(mona.noneOf("abc", true, mona.integer()), "25"),
+                    25);
+        assert.throws(function() {
+          parse(mona.noneOf("abc", true, mona.integer()), "a");
+        }, /expected none of {a,b,c}/);
       });
       it("optionally does a case-insensitive match", function() {
         assert.equal(parse(mona.noneOf("abc", true), "B"), "B");
         assert.throws(function() {
           parse(mona.noneOf("abc", false), "B");
-        }, /expected none of {abc}/);
+        }, /expected none of {a,b,c}/);
       });
     });
     describe("string()", function() {
--- src/mona.js
+++ src/mona.js
@@ -949,24 +949,30 @@ function oneOf(_matches, caseSensitive) {
 }
 
 /**
- * Returns a parser that fails if the next token matches any of the provided
- * `chars`.
+ * Returns a parser that fails if the next token or string matches one of the
+ * given inputs. If the third `parser` argument is given, that parser will be
+ * used to collect the actual value of `noneOf`.
  *
- * @param {String|Array} chars - Character bag to match against.
+ * @param {String|Array} matches - Characters or strings to match. If this
+ *                                 argument is a string, it will be treated as
+ *                                 if matches.split("") were passed in.
  * @param {Boolean} [caseSensitive=true] - Whether to match char case exactly.
+ * @param {Parser} [parser=token()] - What to actually parse if none of the
+ *                                    given matches succeed.
  * @memberof module:mona/strings
  * @instance
  *
  * @example
  * parse(noneOf("abc"), "d"); // => "d"
+ * parse(noneOf(["foo", "bar", "baz"]), "frob"); // => "f"
+ * parse(noneOf(["foo", "bar", "baz"], true, text()), "frob"); // => "frob"
  */
-function noneOf(chars, caseSensitive) {
+function noneOf(_matches, caseSensitive, parser) {
   caseSensitive = typeof caseSensitive === "undefined" ? true : caseSensitive;
-  chars = caseSensitive ? chars : chars.toLowerCase();
-  return label(is(function(x) {
-    x = caseSensitive ? x : x.toLowerCase();
-    return !~chars.indexOf(x);
-  }), "none of {"+chars+"}");
+  var matches = typeof _matches === "string" ? _matches.split("") : _matches;
+  return label(and(not(or.apply(null, matches.map(function(m) {
+    return string(m, caseSensitive);
+  }))), parser || token()), "none of {"+matches+"}");
 }
 
 /**
