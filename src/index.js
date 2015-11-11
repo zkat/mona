34b38d604be4d0b261eaedf1dcb82b1c40e54cab
Kat Marchán <kzm@sykosomatic.org>
2015-11-11 19:04:24 +0900

    get rid of VERSION

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-export const VERSION = '0.8.2'
 export * from '@mona/parse'
 export * from '@mona/parse-async'
 export * from '@mona/core'
 export * from '@mona/combinators'
 export * from '@mona/numbers'
 export * from '@mona/strings'
+// Babel doesn't like export-only files :(
+const x = 1 //eslint-disable-line
